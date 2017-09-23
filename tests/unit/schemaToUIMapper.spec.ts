3381e53f408be728e32fdde07ec3ae52b5e58703
Artur Kasperek <arturmiks@gmail.com>
2017-09-23 23:03:59 +0200

    First idea, tests of project

---
+++ tests/unit/schemaToUIMapper.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import {toMongooseSchema} from "../../src/core/toMongooseSchema";
+import {Schema} from "../../src/interfaces/interfaces";
+import { TEXT_FIELD, NUMBER_FIELD, HTML_FIELD, PASSWORD_FIELD } from '../../src/constans/constans';
+
+describe('Schema to UI Mapper', () => {
+	it('should be able to map basic types', () => {
+
+	});
+});
